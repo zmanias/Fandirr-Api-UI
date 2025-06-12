d796cc9b23e46803dd789feebabc663e03c9510d
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-06-13 01:20:05 +0700

    Update aiimg.js

--- src/api/img creator/aiimg.js
+++ src/api/img creator/aiimg.js
@@ -2,6 +2,7 @@ import express from 'express';
 import axios from 'axios';
 
 module.exports = function(app) {
+app.use(express.json());
 
 const styleList = [
   'default', 'ghibli', 'cyberpunk', 'anime',
