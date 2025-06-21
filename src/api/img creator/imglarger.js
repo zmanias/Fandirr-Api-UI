8a49a3122321a2c454545d211c4272de331957d4
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-06-21 21:20:25 +0700

    update src/api/img creator/imglarger.js

--- src/api/img creator/imglarger.js
+++ src/api/img creator/imglarger.js
@@ -81,8 +81,5 @@ try {
 } catch (err) {
     res.status(500).json({ error: err.message });
 }
-
 });
-
-const PORT = 5000; app.listen(PORT, () => { console.log(✅ Server berjalan di http://localhost:${PORT}); });
-
+}
