b94a1ae7b98b99f5a11281adc700a60197387d4b
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:53:52 +0700

    update src/api/img creator/tourl.js

--- src/api/img creator/tourl.js
+++ src/api/img creator/tourl.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-module.exports = function(app, validateApiKey) {
+module.exports = function(app) {
 const DOMAIN = 'https://api.fandirr.my.id'; // Ganti ke domainmu
 
 const UPLOAD_FOLDER = path.join(__dirname, 'public/uploads');
