9eeb1d859aa015a52135adf162d7fe9767383c5e
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:49:23 +0700

    update src/api/download/saveweb.js

--- src/api/download/saveweb.js
+++ src/api/download/saveweb.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-module.exports = function(app, validateApiKey) {
+module.exports = function(app) {
 
 async function saveweb2zip(url, options = {}) {
     try {
