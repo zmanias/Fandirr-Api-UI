f26e7f4d0e3e683e72c7c374df0514556dc24b30
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:08:48 +0700

    update src/api/panel/delleteall.js

--- src/api/panel/delleteall.js
+++ src/api/panel/delleteall.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
-module.exports = function(app) {
+module.exports = function(app, validateApiKey) {
 
 app.get('/panel/delleteall', async (req, res) => {
   const { domain, plta, pltc, except } = req.query;
