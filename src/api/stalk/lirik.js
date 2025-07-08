feba7f92ce861aa450cd7d1b9dedfa13003ae323
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:13:11 +0700

    update src/api/stalk/lirik.js

--- src/api/stalk/lirik.js
+++ src/api/stalk/lirik.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-module.exports = function(app) {
+module.exports = function(app, validateApiKey) {
 
   // Fungsi scraping lirik
   async function LyricsByPonta(query) {
