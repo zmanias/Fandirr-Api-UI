b23df48f67abdfe379f9f676f6de4c83d679b7bb
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:52:49 +0700

    update src/api/img creator/imglarger.js

--- src/api/img creator/imglarger.js
+++ src/api/img creator/imglarger.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-module.exports = function(app, validateApiKey) {
+module.exports = function(app) {
 
 async function imglarger(buffer, options = {}) { const { scale = '2', type = 'upscale' } = options;
 
