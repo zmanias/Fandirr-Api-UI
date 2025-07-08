12234149fdaff4f20af8f05aa2dbf7165310929c
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:03:31 +0700

    update src/api/img creator/aiimg.js

--- src/api/img creator/aiimg.js
+++ src/api/img creator/aiimg.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 
-module.exports = function(app) {
+module.exports = function(app, validateApiKey) {
 
 const styleList = [
   'default',
