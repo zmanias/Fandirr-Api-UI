a505a91cd795c16098bd83d4a18e2cf2cc738b88
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 12:59:46 +0700

    update src/api/download/an1.js

--- src/api/download/an1.js
+++ src/api/download/an1.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const cheerio = require('cheerio');
 const fetch = require('node-fetch');
 
-module.exports = function(app) {
+module.exports = function(app, validateApiKey) {
 
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36';
 
