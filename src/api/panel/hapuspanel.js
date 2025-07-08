12ee6138f4fada9f7da38a61afcedd9cfef5361b
CyberIndo <55630381+zmanias@users.noreply.github.com>
2025-07-08 13:10:07 +0700

    update src/api/panel/hapuspanel.js

--- src/api/panel/hapuspanel.js
+++ src/api/panel/hapuspanel.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
-module.exports = function(app) {
+module.exports = function(app, validateApiKey) {
 
 app.get('/panel/delpanel', async (req, res) => {
   const { domain, plta, pltc, id } = req.query;
